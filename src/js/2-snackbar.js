ab539bba50103cdc656c34604acf85656081a20d
13DDC <150695013+13DDC@users.noreply.github.com>
2024-05-28 20:39:01 +0300

    done snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,34 @@
-console.log('zbs2');
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+const form = document.querySelector('.form');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const formData = new FormData(form);
+  const timeDelay = formData.get('delay');
+  const state = formData.get('state');
+  const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(timeDelay);
+      } else {
+        reject(timeDelay);
+      }
+    }, timeDelay);
+  });
+  prom
+    .then(timeDelay =>
+      iziToast.success({
+        message: `✅Fulfilled promise in ${timeDelay}ms`,
+        position: 'topRight',
+        icon: '',
+      })
+    )
+    .catch(timeDelay =>
+      iziToast.error({
+        message: `❌ Rejected promise in ${timeDelay}ms`,
+        position: 'topRight',
+        icon: '',
+      })
+    );
+});
